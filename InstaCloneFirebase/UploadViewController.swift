93c7e456b48e094a262663330797a222158eed29
Antoni <porfiriomendoza2509@gamil.com>
2024-09-11 15:52:25 -0600

    Storage para la carga de imagen

--- InstaCloneFirebase/UploadViewController.swift
+++ InstaCloneFirebase/UploadViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseStorage
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -29,11 +31,47 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
-        //saveButton.isEnabled = true
         self.dismiss(animated: true, completion: nil)
     }
 
+    func makeAlert (titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func actionButtonClicked(_ sender: UIButton) {
+        
+        let storage = Storage.storage()
+        let storegeReference = storage.reference()
+        
+        let mediaFolder = storegeReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.3) {
+            
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data, metadata: nil) { metadata, error in
+                
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    imageReference.downloadURL { url, error in
+                        
+                        if error == nil {
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl)
+                            
+                        }
+                    }
+                }
+            }
+            
+        }
+        
     }
     
 
