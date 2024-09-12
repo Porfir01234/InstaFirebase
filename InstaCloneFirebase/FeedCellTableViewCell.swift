8504872b9712cc6aa7ffed11d0cf8e9f40208fe6
Antoni <porfiriomendoza2509@gamil.com>
2024-09-12 17:17:02 -0600

    Recuperando datos de Firebase

---
+++ InstaCloneFirebase/FeedCellTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  FeedCellTableViewCell.swift
+//  InstaCloneFirebase
+//
+//  Created by Jazmin on 12/09/24.
+//
+
+import UIKit
+
+class FeedCellTableViewCell: UITableViewCell {
+    @IBOutlet weak var userEmailLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    @IBOutlet weak var likeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func likeButtonClicked(_ sender: UIButton) {
+    }
+    
+
+}
--- InstaCloneFirebase/FeedViewController.swift
+++ InstaCloneFirebase/FeedViewController.swift
@@ -6,24 +6,83 @@
 //
 
 import UIKit
-
-class FeedViewController: UIViewController {
-
+import FirebaseCore
+import FirebaseFirestoreInternal
+import SDWebImage
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+       
+        tableView.delegate = self
+        tableView.dataSource = self
+       getDataFromFirestore()
+    }
+    func remove (){
+        userEmailArray.removeAll(keepingCapacity: false)
+        userCommentArray.removeAll(keepingCapacity: false)
+        likeArray.removeAll(keepingCapacity: false)
+        userImageArray.removeAll(keepingCapacity: false)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func getDataFromFirestore () {
+        let fireStoreDatabase = Firestore.firestore()
+        /* let settings = fireStoreDatabase.settings
+        settings.areTimestamInSnapshotsEnabled = true
+        fireStoreDatabase.settings = settings */
+        fireStoreDatabase.collection("Posts").addSnapshotListener { snapshot, error in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    
+                      self.remove()
+                    for document in snapshot!.documents {
+                        
+                        
+                        let documentId = document.documentID
+                        
+                        
+                        if let postedBy = document.get("postedBy") as? String {
+                            self.userEmailArray.append(postedBy)
+                        }
+                        if let postedComment = document.get("postComment") as? String {
+                            self.userCommentArray.append(postedComment)
+                        }
+                        if let likes = document.get("likes") as? Int {
+                            self.likeArray.append(likes)
+                        }
+                        if let imageURl = document.get("imageUrl") as? String {
+                            self.userImageArray.append(imageURl)
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+                
+            }
+        }
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userEmailArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCellTableViewCell
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.commentLabel.text = userCommentArray[indexPath.row]
+        cell.userImageView.sd_setImage(with: URL(string: self.userImageArray[indexPath.row]))
+        
+        return cell
+    }
+
 
 }
